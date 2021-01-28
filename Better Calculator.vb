0bad1d1d6fe35dfed7a569d1080b069163759503
Taylor Herndon <TaylorBryanHerndon@gmail.com>
2021-01-27 21:30:07 -0700

    Add project files.

---
+++ Better Calculator.vb
@@ -0,0 +1,166 @@
+﻿Imports System.Threading
+
+Module Module1
+
+    'Declaring a function to be able to get the currently pressed key.
+    'This is needed to not interfere with the normal console interaction.
+    'ie. Console.Readkey.key could not be used because it would overide the normal user inputs.
+
+    Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Short
+
+    Sub Main()
+
+        'Initalizing multithreading to be able to abort the program at ANY point.
+
+        Dim thread1 As Thread = New Thread(New ThreadStart(AddressOf Calculator))
+        Dim thread2 As Thread = New Thread(New ThreadStart(AddressOf Abort))
+
+        'Starting both the calculator and abort processes
+        thread1.Start()
+        thread2.Start()
+
+    End Sub
+
+    'Calculator routine
+    Sub Calculator()
+
+        Dim Number1 As Double = Nothing
+        Dim Number2 As Double = Nothing
+        Dim Operation As String = "~"
+        Dim Result As Double = Nothing
+
+        'Intro lines
+        Console.WriteLine("Hello, welcome to the better calculator!")
+        System.Threading.Thread.Sleep(1000)
+        Console.WriteLine("Here at BetterCalculator Incorperated we pride ourselves on having better calculators than SimpleCalculator Corp.")
+        System.Threading.Thread.Sleep(2000)
+        Console.WriteLine("[Q]: To exit the program at any time.")
+        System.Threading.Thread.Sleep(2000)
+        Console.WriteLine("Press any key to start computing...")
+        Console.ReadKey()
+
+        'Main calculator process
+        Do
+
+            'Get Number 1
+            Do While Number1 = Nothing
+
+                MyActions.PasteTitle(Number1, Number2, Operation, Result)
+                Console.WriteLine(vbNewLine & "Please enter your first number...")
+
+                Try
+
+                    Number1 = Console.ReadLine()
+
+                Catch
+
+                    Number1 = Nothing
+
+                End Try
+
+            Loop
+
+            'Get Number 2
+            Do While Number2 = Nothing
+
+                MyActions.PasteTitle(Number1, Number2, Operation, Result)
+                Console.WriteLine(vbNewLine & "Please enter your second number...")
+
+                Try
+
+                    Number2 = Console.ReadLine()
+
+                Catch
+
+                    Number2 = Nothing
+
+                End Try
+
+            Loop
+
+            'Get desired operation
+            Do Until Operation <> "~"
+
+                MyActions.PasteTitle(Number1, Number2, Operation, Result)
+
+                Console.WriteLine("Now... what do you want for your operation?")
+                Console.WriteLine("1. Addition [+]" & vbNewLine & "2. Subtraction [-]" & vbNewLine & "3. Multiplication [x]" & vbNewLine & "4. Division [÷]")
+
+                Select Case MyActions.ConsoleKeyToNumber()
+                    Case 1
+
+                        Operation = "+"
+                        Result = Number1 + Number2
+
+                    Case 2
+
+                        Operation = "-"
+                        Result = Number1 - Number2
+
+                    Case 3
+
+                        Operation = "x"
+                        Result = Number1 * Number2
+
+                    Case 4
+
+                        Operation = "÷"
+                        Result = Number1 / Number2
+
+                    Case Else
+
+                        Operation = "~"
+
+                End Select
+
+            Loop
+
+            'Ask if the user wants to run the calculator process again
+            MyActions.PasteTitle(Number1, Number2, Operation, Result)
+            Console.WriteLine("Your result should be in the header.")
+            System.Threading.Thread.Sleep(2000)
+            Console.WriteLine("Do you want to compute again?")
+            System.Threading.Thread.Sleep(1000)
+            Console.WriteLine("Y - N")
+
+            Do Until Operation = "~"
+
+                Select Case Console.ReadKey.Key
+
+                    Case ConsoleKey.Y
+
+                        Number1 = Nothing
+                        Number2 = Nothing
+                        Operation = "~"
+                        Result = Nothing
+
+                    Case ConsoleKey.N
+
+                        MyActions.PasteTitle(Number1, Number2, Operation, Result)
+                        Console.WriteLine("Thank you for choosing BetterCalculator Incorperated!")
+                        System.Threading.Thread.Sleep(2000)
+                        Console.WriteLine("Remember to keep on hating SimpleCalculator Corp!")
+                        System.Threading.Thread.Sleep(2000)
+                        End
+
+                End Select
+
+            Loop
+
+        Loop
+
+    End Sub
+
+    Sub Abort()
+
+        'Sleep thread every 100ms until key 81 (Q) is pressed
+        Do Until GetAsyncKeyState(81)
+            Thread.Sleep(100)
+        Loop
+
+        'Once Q is pressed, the entire program is ended.
+        End
+
+    End Sub
+
+End Module
---
+++ MyActions.vb
@@ -0,0 +1,178 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Module MyActions
+
+    Sub PasteTitle(Number1 As Double, Number2 As Double, Operation As String, Result As Double)
+
+        Console.Clear()
+        Console.WriteLine("[--[Welcome to BetterCalculator Incorperated's better calculator TI-X99SRX Titanium Platinum Express]--]")
+        Console.WriteLine("[}===================================================================================================={]")
+        Console.WriteLine("[" & Number1 & "] " & Operation & " [" & Number2 & "] = [" & Result & "]")
+        Console.WriteLine("[}===================================================================================================={]")
+
+
+    End Sub
+
+    Function ConsoleKeyToString()
+
+        Dim ConvertedString As String = ""
+
+        Select Case Console.ReadKey.Key
+            Case ConsoleKey.Enter
+                ConvertedString = "Enter"
+            Case ConsoleKey.D1
+                ConvertedString = "1"
+            Case ConsoleKey.D2
+                ConvertedString = "2"
+            Case ConsoleKey.D3
+                ConvertedString = "3"
+            Case ConsoleKey.D4
+                ConvertedString = "4"
+            Case ConsoleKey.D5
+                ConvertedString = "5"
+            Case ConsoleKey.D6
+                ConvertedString = "6"
+            Case ConsoleKey.D7
+                ConvertedString = "7"
+            Case ConsoleKey.D8
+                ConvertedString = "8"
+            Case ConsoleKey.D9
+                ConvertedString = "9"
+            Case ConsoleKey.D0
+                ConvertedString = "0"
+            Case ConsoleKey.NumPad1
+                ConvertedString = "1"
+            Case ConsoleKey.NumPad2
+                ConvertedString = "2"
+            Case ConsoleKey.NumPad3
+                ConvertedString = "3"
+            Case ConsoleKey.NumPad4
+                ConvertedString = "4"
+            Case ConsoleKey.NumPad5
+                ConvertedString = "5"
+            Case ConsoleKey.NumPad6
+                ConvertedString = "6"
+            Case ConsoleKey.NumPad7
+                ConvertedString = "7"
+            Case ConsoleKey.NumPad8
+                ConvertedString = "8"
+            Case ConsoleKey.NumPad9
+                ConvertedString = "9"
+            Case ConsoleKey.NumPad0
+                ConvertedString = "0"
+            Case ConsoleKey.A
+                ConvertedString = "A"
+            Case ConsoleKey.B
+                ConvertedString = "B"
+            Case ConsoleKey.C
+                ConvertedString = "C"
+            Case ConsoleKey.D
+                ConvertedString = "D"
+            Case ConsoleKey.E
+                ConvertedString = "E"
+            Case ConsoleKey.F
+                ConvertedString = "F"
+            Case ConsoleKey.G
+                ConvertedString = "G"
+            Case ConsoleKey.H
+                ConvertedString = "H"
+            Case ConsoleKey.I
+                ConvertedString = "I"
+            Case ConsoleKey.J
+                ConvertedString = "J"
+            Case ConsoleKey.K
+                ConvertedString = "K"
+            Case ConsoleKey.L
+                ConvertedString = "L"
+            Case ConsoleKey.M
+                ConvertedString = "M"
+            Case ConsoleKey.N
+                ConvertedString = "N"
+            Case ConsoleKey.O
+                ConvertedString = "O"
+            Case ConsoleKey.P
+                ConvertedString = "P"
+            Case ConsoleKey.Q
+                ConvertedString = "Q"
+            Case ConsoleKey.R
+                ConvertedString = "R"
+            Case ConsoleKey.S
+                ConvertedString = "S"
+            Case ConsoleKey.T
+                ConvertedString = "T"
+            Case ConsoleKey.U
+                ConvertedString = "U"
+            Case ConsoleKey.V
+                ConvertedString = "V"
+            Case ConsoleKey.W
+                ConvertedString = "W"
+            Case ConsoleKey.X
+                ConvertedString = "X"
+            Case ConsoleKey.Y
+                ConvertedString = "Y"
+            Case ConsoleKey.Z
+                ConvertedString = "Z"
+            Case ConsoleKey.Spacebar
+                ConvertedString = " "
+            Case Else
+                ConvertedString = ""
+        End Select
+
+        Return ConvertedString
+
+    End Function
+
+    Function ConsoleKeyToNumber()
+
+        Dim ConvertedString As Integer = Nothing
+
+        Select Case Console.ReadKey.Key
+            Case ConsoleKey.D1
+                ConvertedString = 1
+            Case ConsoleKey.D2
+                ConvertedString = 2
+            Case ConsoleKey.D3
+                ConvertedString = 3
+            Case ConsoleKey.D4
+                ConvertedString = 4
+            Case ConsoleKey.D5
+                ConvertedString = 5
+            Case ConsoleKey.D6
+                ConvertedString = 6
+            Case ConsoleKey.D7
+                ConvertedString = 7
+            Case ConsoleKey.D8
+                ConvertedString = 8
+            Case ConsoleKey.D9
+                ConvertedString = 9
+            Case ConsoleKey.D0
+                ConvertedString = 0
+            Case ConsoleKey.NumPad1
+                ConvertedString = 1
+            Case ConsoleKey.NumPad2
+                ConvertedString = 2
+            Case ConsoleKey.NumPad3
+                ConvertedString = 3
+            Case ConsoleKey.NumPad4
+                ConvertedString = 4
+            Case ConsoleKey.NumPad5
+                ConvertedString = 5
+            Case ConsoleKey.NumPad6
+                ConvertedString = 6
+            Case ConsoleKey.NumPad7
+                ConvertedString = 7
+            Case ConsoleKey.NumPad8
+                ConvertedString = 8
+            Case ConsoleKey.NumPad9
+                ConvertedString = 9
+            Case ConsoleKey.NumPad0
+                ConvertedString = 0
+            Case Else
+                ConvertedString = Nothing
+        End Select
+
+        Return ConvertedString
+
+    End Function
+
+End Module
